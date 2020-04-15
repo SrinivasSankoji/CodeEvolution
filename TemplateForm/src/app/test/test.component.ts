76b00075206d96f29d39168dcbfb16ea19dceb58
srinivassankoji <srinivaschary.chary@gmail.com>
2020-04-16 01:37:20 +0530

    Enroll Service

--- TemplateForm/src/app/test/test.component.ts
+++ TemplateForm/src/app/test/test.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { EnrollmentService } from '../enrollment.service';
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-test',
@@ -8,12 +10,34 @@ import { User } from '../user';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private enrollmentService:EnrollmentService) { }
 
   ngOnInit(): void {
   }
 
   topics =['Angular','Javascript','Forms'];
 
-  userModel=new User('Bhaumik','bcs@gmail.com','7718973681','','Morning',true);
+  userModel=new User('Bhaumik','bcs@gmail.com','7718973681','default','Morning',true);
+
+  topicHasError=true;
+
+  public validateTopic(value)
+  {
+    if(value ==='default')
+    {
+      this.topicHasError=true;
+    }
+    else
+    {
+      this.topicHasError=false;
+    }
+  }
+
+  public onSubmit()
+  {
+    this.enrollmentService.enroll(this.userModel)
+    .subscribe(data => console.log('Success!',data),
+               error => console.log('Error!',error)
+    );
+  }
 }
