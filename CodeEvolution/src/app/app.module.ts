3a2555fccbe761187d55721272353e8dfa5618f3
srinivassankoji <srinivaschary.chary@gmail.com>
2020-04-24 02:29:04 +0530

    Routing Changes

--- CodeEvolution/src/app/app.module.ts
+++ CodeEvolution/src/app/app.module.ts
@@ -13,15 +13,10 @@ import { PropertybindingComponent } from './propertybinding/propertybinding.comp
 import { EventbindingComponent } from './eventbinding/eventbinding.component';
 import { TemplatereferenceComponent } from './templatereference/templatereference.component';
 import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
-import { from } from 'rxjs';
 import { ParentchildComponent } from './parentchild/parentchild.component';
-import { EmployeelistComponent } from './employeelist/employeelist.component';
 import { EmployeedetailComponent } from './employeedetail/employeedetail.component';
 import { ObservableComponent } from './observable/observable.component';
 import { RoutingComponent } from './routing/routing.component';
-import { DepartmentlistComponent } from './departmentlist/departmentlist.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 
 @NgModule({
   declarations: [
@@ -36,9 +31,7 @@ import { DepartmentDetailComponent } from './department-detail/department-detail
     EmployeedetailComponent,
     ObservableComponent,
     RoutingComponent,
-    routingComponents,
-    PagenotfoundComponent,
-    DepartmentDetailComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
