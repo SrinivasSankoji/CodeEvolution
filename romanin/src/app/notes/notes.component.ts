d1ff2f8fd96bb6e6618a35a39c53b108125f5a72
srinivassankoji <srinivaschary.chary@gmail.com>
2020-05-05 01:21:04 +0530

    Romanian

--- romanin/src/app/notes/notes.component.ts
+++ romanin/src/app/notes/notes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Notebook } from './model/notebook';
 import { NoteserviceService } from '../service/noteservice.service';
+import { Note } from './model/note';
 
 @Component({
   selector: 'app-notes',
@@ -11,23 +11,43 @@ import { NoteserviceService } from '../service/noteservice.service';
 export class NotesComponent implements OnInit {
 
   notebooks:Notebook[]=[];
+  notes:Note[]=[];
+  selectedNotebook:Notebook;
 
   constructor(private _notesService:NoteserviceService) { }
 
     ngOnInit() {
       this.getAllNoteBooks();
+      this.getAllNotes();
     }
 
     public getAllNoteBooks()
     {
-      let url="http://localhost:1235//api/notebooks/all";
-      this._notesService.getAllNoteBooks().subscribe(
-        (response) =>{
+      this._notesService.getAllNoteBooks().subscribe((response) =>{
           this.notebooks=response;
       },(error) =>{
-
+        alert("Unable to get all the NoteBooks");
       });
     }
+
+    public getAllNotes()
+    {
+        this._notesService.getAllNotes().subscribe((response) =>{
+        this.notes = response;
+        },(error) =>{
+          alert("Unable to get all the Notes");
+        });
+    }
+
+    /**public getNotesByNotebookId()
+    {
+      this._notesService.getNotesByNotebookId().subscribe((response)=>{
+
+      },(error) =>{
+        alert("Error while Geeting the Notes by Id");
+      });
+    }**/
+
 
     public createNotebook()
     {
@@ -36,7 +56,7 @@ export class NotesComponent implements OnInit {
         id:null,
         nbNotes:0
       }
-      this._notesService.postNotebook(newNotebook).subscribe((response) =>{
+      this._notesService.saveNotebook(newNotebook).subscribe((response) =>{
         newNotebook.id=response.id;
         this.notebooks.push(newNotebook);
       },(error) =>{
@@ -44,13 +64,39 @@ export class NotesComponent implements OnInit {
       })
     }
 
+    public createNote(notebookId:string)
+    {
+        let newNote:Note ={
+          id:null,
+          title:'New Notes',
+          text:'Creating Notes',
+          notebookId:notebookId,
+          lastModifiedOn:null
+        }
+        this._notesService.saveNote(newNote).subscribe((response)=>{
+          newNote.id=response.id;
+          this.notes.push(newNote);
+        },(error)=>{
+          alert("Error While Creating Note");
+        });
+    }
+
     public updateNotebook(updatedNotebook:Notebook)
     {
-      this._notesService.postNotebook(updatedNotebook).subscribe((response) =>{
-
+      this._notesService.saveNotebook(updatedNotebook).subscribe((response) =>{
+        alert("Notebook Name has Updated Successfully")
       },(error) =>{
         alert("Error While Updating the Notebook");
       })
+    }
+
+    public updateNote(updatedNote:Note)
+    {
+      this._notesService.saveNote(updatedNote).subscribe((response)=>{
+        alert("Notes has Updated Successfully");
+      },(error)=>{
+        alert("Error While Updating Note ");
+      });
     }
 
     public deleteNotebook(notebook:Notebook)
@@ -61,9 +107,39 @@ export class NotesComponent implements OnInit {
               let index=this.notebooks.indexOf(notebook);
               this.notebooks.splice(index,1);
             },(error) =>{
-              alert("Unable to Delete the Record");
+              alert("Unable to Delete the Notebook");
             })
         }
     }
 
+    public deleteNote(note:Note)
+    {
+      if(confirm("Are You Sure You want to Delete the Notebook"))
+      {
+        alert(note.id);
+        this._notesService.deleteNote(note.id).subscribe((response) =>{
+          let index=this.notes.indexOf(note);
+          this.notes.splice(index,1);
+        },(error) =>{
+          alert("Unable to Delete the Note");
+        })
+      }
+
+    }
+
+   public selectNotebook(notebook:Notebook)
+   {
+      this.selectedNotebook=notebook;
+      this._notesService.getNotesByNotebookId(notebook.id).subscribe((response) =>{
+        this.notes=response;
+      },(error) =>{
+        alert("Error has Occured while Downloading the Notes");
+      });
+   }
+
+   public selectAllNotes()
+   {
+      this.selectedNotebook=null;
+      this.getAllNotes();
+   }
 }
--- romanin/src/app/service/noteservice.service.ts
+++ romanin/src/app/service/noteservice.service.ts
@@ -3,32 +3,52 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Notebook } from '../notes/model/notebook';
 import { FeedbackViewModel } from '../feedback/feedback.component';
+import { Note } from '../notes/model/note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteserviceService {
 
-  private baseurl="http://localhost:1235//api/";
+  private baseurl="http://localhost:1235/api/";
 
   constructor(private _http:HttpClient) { }
 
   public getAllNoteBooks():Observable<Notebook[]>
   {
-    return this._http.get<Notebook[]>(this.baseurl+'notebooks/all');
+    return this._http.get<Notebook[]>(this.baseurl+'notebook/getAllNoteBooks');
+  }
+
+  public getAllNotes():Observable<Note[]>
+  {
+    return this._http.get<Note[]>(this.baseurl+'/notes/getAllNotes');
+  }
+
+  public saveNotebook(notebook:Notebook):Observable<Notebook>
+  {
+   return this._http.post<Notebook>(this.baseurl+'/notebook/saveNotebook',notebook);
+  } 
+
+  public saveNote(note:Note):Observable<Note>{
+    return this._http.post<Note>(this.baseurl+'/notes/saveNotes',note);
+  }
+
+  public deleteNotebook(id:string):Observable<any>
+  {
+    return this._http.delete(this.baseurl+'/notebook/'+id);
+  }
+
+  public deleteNote(noteId:string):Observable<any>
+  {
+    return this._http.delete(this.baseurl+'/notes/'+noteId);
+  }
+  
+  public getNotesByNotebookId(notebookId:string):Observable<Note[]>
+  {
+    return this._http.get<Note[]>(this.baseurl+'/notes/byNotebook/'+notebookId);
   }
 
   public postFeedback(feedback:FeedbackViewModel):Observable<any>{
     return this._http.post<any>(this.baseurl+'feedback',feedback);
-  }
-
-  public postNotebook(notebook:Notebook):Observable<Notebook>
-  {
-   return this._http.post<Notebook>(this.baseurl+'/notebooks',notebook);
-  } 
-
-  public deleteNotebook(id:string):Observable<any>
-  {
-    return this._http.delete(this.baseurl+'/notebooks/'+id);
   }
 }
