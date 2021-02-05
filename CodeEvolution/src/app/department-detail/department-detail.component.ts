a7350aeb1d835a8ff7aff5aa498cf99329e19fc4
srinivassankoji <srinivaschary.chary@gmail.com>
2021-02-05 12:22:50 +0530

    Copy

--- CodeEvolution/src/app/department-detail/department-detail.component.ts
+++ CodeEvolution/src/app/department-detail/department-detail.component.ts
@@ -11,13 +11,12 @@ export class DepartmentDetailComponent implements OnInit {
   public departmentId;
   constructor(private activatedRoute:ActivatedRoute,private router:Router) 
   { 
-
   }
 
   ngOnInit(): void {
     /**Route Parameter to read the Parameter from the URL */
-    //let id=parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
-    //this.departmentId=id;
+   /**  let id=parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.departmentId=id;**/
     /** Param Map Observable to read the Route Parameter from the URL */
     this.activatedRoute.paramMap.subscribe((params :ParamMap) => {
       let id=parseInt(params.get('id'));
@@ -27,20 +26,23 @@ export class DepartmentDetailComponent implements OnInit {
 
   goPrevious()
   {
+    console.log("Previous Department "+this.departmentId);
       let previousId=this.departmentId-1;
       this.router.navigate(['/departmentdetail',previousId]);
   }
   goNext()
   {
+    console.log("Next Department "+this.departmentId);
     let nextId=this.departmentId+1;
     this.router.navigate(['/departmentdetail',nextId]);
   }
 
-  public goToDepartment()
+  goToDepartment()
   {
-    let selectedId = this.departmentId ? this.departmentId : null;
-    //this.router.navigate(['/departmentlist', {id: selectedId}]);
-    this.router.navigate(['../',{id: selectedId}],{relativeTo:this.activatedRoute});
+    console.log("Selected Department "+this.departmentId);
+    let selectedId = this.departmentId;
+    this.router.navigate(['/departmentlist', {id: selectedId}]);
+    //this.router.navigate(['../',{id: selectedId}],{relativeTo:this.activatedRoute});
     //Go Back One Segment in the URl
   }
 
